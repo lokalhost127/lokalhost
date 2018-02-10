3ff04c032adde340cd756e89727a41c32b418d07
lokal3 <ivannar995@gmail.com>
2018-02-10 23:30:15 +0100

    kreiranje na brojche za masi and stuff

--- resources/views/locations/create.blade.php
+++ resources/views/locations/create.blade.php
@@ -147,19 +147,19 @@
 
 
             <!-- The Modal -->
-            <div class="modal fade" id="myModal">
+            <div class="modal fade" id="myModal" data-backdrop="static" data-keyboard="false">
                 <div class="modal-dialog">
                     <div class="modal-content">
 
                         <!-- Modal Header -->
                         <div class="modal-header">
                             <h4 class="modal-title">Креирај на локација?</h4>
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <button type="button" v-on:click="deleteNumbers" class="close" data-dismiss="modal">&times;</button>
                         </div>
 
                         <!-- Modal footer -->
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" data-dismiss="modal">Откажи</button>
+                            <button type="button" v-on:click="deleteNumbers" class="btn btn-primary" data-dismiss="modal">Откажи</button>
                             <button type="submit" class="btn btn-danger">Прифати</button>
                         </div>
 
